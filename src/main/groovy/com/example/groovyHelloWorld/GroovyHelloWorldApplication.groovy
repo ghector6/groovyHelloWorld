d3a60d0e6eab09f2ec4f35a960802b6d8b6f9226
Hector Garcia Rosales <ghectormus@gmail.com>
2022-09-15 22:47:24 -0500

    Adding this from cli

--- src/main/groovy/com/example/groovyHelloWorld/GroovyHelloWorldApplication.groovy
+++ src/main/groovy/com/example/groovyHelloWorld/GroovyHelloWorldApplication.groovy
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class GroovyHelloWorldApplication {
+class GroovyHelloWorldApplication { //prueba cli
 
 	static void main(String[] args) {
 		SpringApplication.run(GroovyHelloWorldApplication, args)
